3b81568670f4b5cebdab6b834038d5bf695ade96
Peter Thompson <peterthompson@roamflyfishing.com>
2018-04-08 14:43:23 -0700

    Implemented jquery function to dynamically add boards to home page

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -27,51 +27,85 @@ var pinterestLogin = function(){
 }
 
 function pintrest(){
-        window.pAsyncInit = function() {
-            PDK.init({
-                appId: "4959715914598591625",
-                cookie: true
-            });
-            //login
-            PDK.login({ scope : 'read_relationships,read_public' }, function(response){
-                if (!response || response.error) {
-                  //  alert('Error occurred');
-                } else {
-                   //console.log(JSON.stringify(response));
-                }
-            //get board info
+  window.pAsyncInit = function() {
+      PDK.init({
+          appId: "4959715914598591625",
+          cookie: true
+      });
+      //login
+      PDK.login({ scope : 'read_relationships,read_public' }, function(response){
+          if (!response || response.error) {
+            //  alert('Error occurred');
+          } else {
+             //console.log(JSON.stringify(response));
+          }
+      //get board info
 
-            var pins = [];
+      var pins = [];
+      console.log("Response:", response);
 
-            PDK.request('/v1/me/', function (response) {
-              if (!response || response.error) {
-                //alert('Error occurred');
-              } else {
-              	// console.log(JSON.stringify(response));
-                  //  alert('success');
-                    // console.log(PDK.getSession().accessToken);
+      PDK.me("pins", function(response){
+        console.log("pdk.me:", response)
+      })
+      PDK.request('/v1/me/', function (response) {
+        if (!response || response.error) {
+          alert('Error occurred');
+        } else {
+          var boardItem = "";
+          console.log(response.data);
 
-                    var yahoo = $( "#result" ).load( "https://api.pinterest.com/v1/me/?access_token="+PDK.getSession().accessToken+"&fields=counts" );
+          $.ajax({
+            url:  "https://api.pinterest.com/v1/me/boards/?access_token="+PDK.getSession().accessToken+"&fields=id,name,url,description,image"
+          }).done(function(response){
+            response.data.forEach(function(board){
 
-                    console.log(yahoo['data'])
-                    // var board = yahoo['counts']['boards'];
-                    // $('.board').append(board);
-                    // PDK.logout();
-              }
-            });
+              var id = board.id;
+              var name = board.name;
+              var url = board.url;
+              var description = board.description;
+              var image = board.image["60x60"].url;
+              console.log(id);
+              console.log(name);
+              console.log(url);
+              console.log(description);
+              console.log(image);
+              var boardItem = boardItem + "<div class='bg-light text-center mr-md-3 board-items pt-3 px-3 pt-md-5 px-md-5 text-centers'>"
+                                          + "<div class='my-3 p-3'>"
+                                            + "<h2 class='display-5'>" + name + "</h2>"
+                                            + "<p class='lead'>" + description + "</p>"
+                                          + "</div>"
+                                          + "<div class='image-div' style='width: 80%; height: 300px; border-radius: 21px 21px 0 0;'>"
+                                            + "<img class='board-image' src='" + image + "'/>"
+                                          + "</div>"
+                                        + "</div>";
+              $('.my-md-3').append(boardItem);
+              // <div class="bg-light text-center mr-md-3 board-items pt-3 px-3 pt-md-5 px-md-5 text-centers">
+              //   <div class="my-3 p-3">
+              //     <h2 class="display-5">Another headline</h2>
+              //     <p class="lead">And an even wittier subheading.</p>
+              //   </div>
+              //   <div class="image-div" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+              // </div>
+            })
+            console.log(response.data)
+            // $('.image-div').prepend('<img class="board-image" src=' + response.data[0].image.url + ' />')
+          }).fail(function(error){
+            console.log(error)
+          })
+          // PDK.logout();
+        }
+      });
+      //end get board info
+      });
+      //end login
+  };
 
-            //end get board info
-            });
+  (function(d, s, id){
+      var js, pjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "//assets.pinterest.com/sdk/sdk.js";
+      pjs.parentNode.insertBefore(js, pjs);
+  }(document, 'script', 'pinterest-jssdk'));
 
-            //end login
-        };
-
-        (function(d, s, id){
-            var js, pjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement(s); js.id = id;
-            js.src = "//assets.pinterest.com/sdk/sdk.js";
-            pjs.parentNode.insertBefore(js, pjs);
-        }(document, 'script', 'pinterest-jssdk'));
-
-      }
+}
