414db006f6934828078a15186960b6dffcbbcb80
Peter Thompson <peterthompson@roamflyfishing.com>
2018-04-12 19:28:53 -0700

    Changed whitelisted ip

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module PinIt
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-    config.web_console.whitelisted_ips = '192.168.0.0'
+    config.web_console.whitelisted_ips = '127.0.0.0'
     config.web_console.whiny_requests = false
     config.web_console.development_only = false
   end
